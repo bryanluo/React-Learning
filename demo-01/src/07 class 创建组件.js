4d0166d7bc31b80d66d59541821f3e0cdcbe30e4
shijiang.luo <shijiang.luo@gziiim.com>
2020-12-31 11:12:31 +0800

    添加 class 创建组件的相关代码

--- demo-01/src/07 class 创建组件.js
+++ demo-01/src/07 class 创建组件.js
@@ -2,9 +2,9 @@ import React,{Component} from "react";
 import ReactDOM from "react-dom";
 
 // 基于 class 关键字创建组件
-
 class SayHello extends Component {
   // 在组件内部必须有 reder 函数，render 函数必须返回合法的 JSX 虚拟DOM结构
+  // 作用：渲染 DOM 结构
   render() {
     return <div>这是一个 class 创建的组件</div>;
   }
