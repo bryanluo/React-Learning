2002c9ff14d4794b0702c4cb73cae074e1b98d29
shijiang.luo <shijiang.luo@gziiim.com>
2020-12-31 15:31:27 +0800

    1、使用行内样式实现对标签渲染 2、将样式封装成一个对象 3、将所有样式抽离出来并封装到js文件中

--- demo-01/src/components/CmtList.jsx
+++ demo-01/src/components/CmtList.jsx
@@ -10,7 +10,7 @@ import {CmtItem} from "@/components/CmtItem";
 export default class CmtList extends React.Component{
     constructor(){
         super()
-        this.state = {
+        this.state = { 
             list : [
                 {id: 1, user: "zhangsan", content:"haha"},
                 {id: 2, user: "lisi", content:"wocao"},
@@ -18,12 +18,12 @@ export default class CmtList extends React.Component{
                 {id: 4, user: "zhaoliu", content:"仇尼扎的"},
                 {id: 5, user: "tianqi", content:"瞅你咋地"}
             ]
-        }
+        } 
     }
 
     render(){
         return <div>
-            <h1>这是评论列表组件</h1>
+            <h1 style={{ color: 'red', fontSize: '35px', zIndex: 3, fontWeight: 200, textAlign: 'center' }}>这是评论列表组件</h1>
             {this.state.list.map(item => <CmtItem key={item.id} {...item}></CmtItem>)}
         </div>
     }
